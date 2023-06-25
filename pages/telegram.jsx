08a8ee2f952ab496b9f37e978d4980f9a4b4d8e7
alinaderi <malinaderi1@outlook.com>
2023-06-25 14:43:45 +0330

    edit

---
+++ pages/telegram.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const telegram = () => {
+  return (
+    <div>
+      این ادرس تلگرا من هست بنظرم از اینجا با هم در ارتباط باشیم بهتر هست
+      <a href="https://t.me/alinaderi17">telegram</a>
+    </div>
+  );
+};
+
+export default telegram;
